776b6b43beb77fdb3ef2fde29d53a13e9df4ae3c
Ryan W <47390291+onlinePB@users.noreply.github.com>
2021-10-12 03:34:52 +0100

    Set up Pygame

--- snake.py
+++ snake.py
@@ -4,3 +4,13 @@ WIN_WIDTH = 800
 WIN_HEIGHT = 800
 PIXEL_SIZE = 20
 
+
+def main():
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    pygame.display.set_caption("Snake")
+
+if __name__ == "__main__":
+    main()
